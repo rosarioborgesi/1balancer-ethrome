68c6c08325bcde2184a75a6dd41622c998cc4598
pf55351 <francesco.primerano79@gmail.com>
2025-10-18 20:30:42 +0200

    add new hookj

--- hooks/use-get-portofolio.ts
+++ hooks/use-get-portofolio.ts
@@ -1,11 +1,14 @@
+import { REBALANCER_WALLET } from "@/config/constants";
+import { getTokensSnapshot } from "@/lib/actions/portfolio";
 import { useQuery } from "@tanstack/react-query";
+import { base } from "viem/chains";
 
 const REFETCH_INTERVAL = 5000;
 
 export const useGetPorftoflio = () => {
   const { data, isSuccess, isLoading } = useQuery({
     queryKey: ["get-portfolio"],
-    queryFn: () => {},
+    queryFn: () => getTokensSnapshot(base.id.toString(), REBALANCER_WALLET),
     refetchInterval: REFETCH_INTERVAL,
   });
 
