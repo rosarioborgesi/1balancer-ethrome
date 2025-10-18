c038e577a279b6d7fac0a9f7ccb909ba8c612045
pf55351 <francesco.primerano79@gmail.com>
2025-10-18 18:45:57 +0200

    add new function to format address

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatAddress(
+  address: string | undefined,
+  startChars = 6,
+  endChars = 4
+): string {
+  if (!address) return "";
+  return `${address.slice(0, startChars)}...${address.slice(-endChars)}`;
+}
